e25e2138a4919401538a51cf7e3f9c3893c47b4c
Nick McSpadden <nmcspadden@gmail.com>
2021-10-04 20:43:31 -0700

    Adding Smithing and Crafting

---
+++ crafting.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+
+import json
+import itertools
+
+leather_items = [
+    "Gloves",
+    "Boots",
+    "Cowl",
+    "Vambraces",
+    "Body",
+    "Chaps",
+]
+
+leather_materials = [
+    "Leather",
+    "Hard Leather"
+]
+
+dhide_items = [
+    "Vambraces",
+    "Chaps",
+    "Shield",
+    "Body"
+]
+
+dhide_materials = [
+    "Green D-hide",
+    "Blue D-hide",
+    "Red D-hide",
+    "Black D-hide"
+]
+
+jewels_items = [
+    "Topaz",
+    "Sapphire",
+    "Ruby",
+    "Emerald",
+    "Diamond"
+]
+
+jewelry_types = [
+    "Ring",
+    "Necklace"
+]
+
+rings_materials = [
+    "Silver",
+    "Gold"
+]
+
+mastery_item = [
+    "Mastery Level",
+    "Crafting",
+    "",
+    "CURRENT_ITEM",
+    "99",
+    [
+        [
+            "Start Skill",
+            "Crafting",
+            "NEXT_ITEM",
+            "",
+            ""
+        ]
+    ]
+]
+
+def itemize(*args):
+    if len(args) == 3:
+        return [" ".join((x, y, z)) for (x, y, z) in itertools.product(*args)]
+    return [" ".join((x, y)) for (x, y) in itertools.product(*args)]
+
+
+def generate_list(itemlist):
+    mastery_list = []
+    # Iterate through each pair
+    for grouping in itertools.pairwise(itemlist):
+        mastery_list.append(
+            [
+                "Mastery Level",
+                "Crafting",
+                "",
+                grouping[0],
+                "99",
+                [
+                    [
+                        "Start Skill",
+                        "Crafting",
+                        grouping[1],
+                        "",
+                        ""
+                    ]
+                ]
+            ]    
+        )
+    return mastery_list
+
+mastery_list = []
+itemlist = []
+itemlist.extend(itemize(leather_materials, leather_items))
+itemlist.extend(itemize(dhide_materials, dhide_items))
+itemlist.extend(itemize(rings_materials, jewels_items, jewelry_types))
+mastery_list.extend(generate_list(itemlist))
+# # Combine all elements
+# jewelry_list = [" ".join((x, y, z)) for (x, y, z) in itertools.product(rings_materials, jewels_items, jewelry_types)]
+
+# Manually add the final one
+mastery_list.append(
+    [
+        "Mastery Level",
+        "Smithing",
+        "",
+        "Gold Diamond Necklace",
+        "99",
+        [
+            [
+                "Start Skill",
+                "Agility",
+                "",
+                "",
+                ""
+            ]
+        ]
+    ]
+)
+
+with open("crafting_mastery.txt", "w") as f:
+    json.dump(mastery_list, f, sort_keys=False, indent=2)
---
+++ smithing.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+
+import json
+import itertools
+
+items = [
+    "Dagger",
+    "Throwing Knife",
+    "Sword",
+    "Arrowtips",
+    "Gloves",
+    "Scimitar",
+    "Helmet",
+    "Battleaxe",
+    "Javelin Heads",
+    "Boots",
+    "Shield",
+    "2H Sword",
+    "Crossbow Head",
+    "Platelegs",
+    "Platebody"
+]
+
+materials = [
+    "Bronze",
+    "Iron",
+    "Steel",
+    "Mithril",
+    "Adamant",
+    "Rune",
+    "Dragon"
+]
+
+mastery_item = [
+    "Mastery Level",
+    "Smithing",
+    "",
+    "CURRENT_ITEM",
+    "99",
+    [
+        [
+            "Start Skill",
+            "Smithing",
+            "NEXT_ITEM",
+            "",
+            ""
+        ]
+    ]
+]
+
+item_tuples = itertools.product(materials, items)
+itemlist = []
+for item in item_tuples:
+    itemlist.append(item[0] + " " + item[1])
+
+mastery_list = []
+# Iterate through each pair
+for grouping in itertools.pairwise(itemlist):
+    mastery_list.append(
+        [
+            "Mastery Level",
+            "Smithing",
+            "",
+            grouping[0],
+            "99",
+            [
+                [
+                    "Start Skill",
+                    "Smithing",
+                    grouping[1],
+                    "",
+                    ""
+                ]
+            ]
+        ]    
+    )
+
+# Manually add the final one
+mastery_list.append(
+    [
+        "Mastery Level",
+        "Smithing",
+        "",
+        itemlist[-1:][0],
+        "99",
+        [
+            [
+                "Start Skill",
+                "Agility",
+                "",
+                "",
+                ""
+            ]
+        ]
+    ]
+)
+
+with open("smithing_mastery.txt", "w") as f:
+    json.dump(mastery_list, f, sort_keys=False, indent=2)
